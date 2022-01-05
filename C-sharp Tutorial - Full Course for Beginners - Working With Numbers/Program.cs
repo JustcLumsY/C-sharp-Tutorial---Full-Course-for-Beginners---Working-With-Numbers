58f408ac58ac991ba9c04e690e4cd719c7d11c40
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 12:15:17 +0100

    Add project files.

---
+++ C-sharp Tutorial - Full Course for Beginners - Working With Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace C_sharp_Tutorial___Full_Course_for_Beginners___Working_With_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+           
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+    }
+}
